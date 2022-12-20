99ce49c9f7e9b3a301d8a8d52acac6999215c987
michael Ivanushkin <Michaeli106@nycstudents.net>
2022-12-20 13:13:38 -0500

    her

---
+++ vite-project/js/main.js
@@ -0,0 +1,8 @@
+import '../styles/style.css'
+
+document.querySelector('#app').innerHTML = `
+<h1>Hello Vite!!!!1</h1>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>`
+    ;
